750e25c6821c2f2d0007390e1c6c2ce3ad22d107
Shal Malhotra <smalhotra0109@gmail.com>
2023-10-03 18:19:32 -0400

    control flow lab changes

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  let isFree = false;
+  if (someValue <= 400) {
+    isFree = true;
+  } else if (someValue > 2500) {
+    return 'No can do.';
+  } else if (someValue >= 400 && someValue <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  return isFree === true ? 'This one is on me!' : 'I will gladly take your thirty bucks.';
+}
+function ternaryCheckCity(someCity) {
+  let city = 'NYC';
+
+  if (someCity === city) {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'thanks for everything':
+      return 'Bye.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Thank you.';
+  }
 }
